63aa66db5a58d089cf0d9db9bc7ce11441990817
Tiago <tiagodev@protonmail.com>
2023-02-09 00:48:36 -0300

    using mw

--- server.js
+++ server.js
@@ -4,26 +4,41 @@ const PORT = 4000
 
 // middleware
 app.use(express.json())
+app.use(express.static('public'))
+
+function middleware(req, res, next) {
+  console.log('HIT THE MIDDLEWARE')
+  next()
+}
+
+// TEMP DATABASE
+const db = []
 
 // http://172.0.0.1/path-here
 
-app.get('/', (req, res) => {
-  console.log('you are in Home route!: GET')
-  res.status(201).json({"message": 'Welcome to the json response'})
-})
+// no need because of 'express.static()'
+// app.get('/', (req, res) => {
+//   console.log('you are in Home route!: GET')
+//   res.sendStatus(200)
+// })
 
-app.post('/api/info', (req, res) => {
-  const {information} = req.body
+app.post('/api/info', middleware, (req, res) => {
+  const { information } = req.body
   console.log('THE POSTED MESSAGE WAS:', information)
+  db.push(information)
+  console.log('DB', db)
   res.status(201).json({"yourMessage": information})
 })
 
-app.put('/api', (req, res) => {
-  res.send('putting something')
+app.put('/api', middleware, (req, res) => {
+  const { word, potato } = req.query
+  console.log(word, potato)
+  res.sendStatus(200)
 })
 
-app.delete('/delete/:id', (req, res) => {
-  console.log('Want to delete something ?')
+app.delete('/delete/:id/:name', middleware, (req, res) => {
+  const { id, name } = req.params
+  console.log('want to delete what? ', id, name)
   res.sendStatus(200)
 })
 
