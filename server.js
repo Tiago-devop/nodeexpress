66ef70c7e88d4776c039aa31ee74ac687c99f2c5
Tiago <tiagodev@protonmail.com>
2023-02-08 23:51:07 -0300

    adding routes to test them and using 'use' from express to not receive a body undefined

--- server.js
+++ server.js
@@ -2,13 +2,29 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
+// middleware
+app.use(express.json())
+
+// http://172.0.0.1/path-here
+
 app.get('/', (req, res) => {
   console.log('you are in Home route!: GET')
-  res.status(201).send('Hello')
+  res.status(201).json({"message": 'Welcome to the json response'})
 })
 
-app.delete('/', (req, res) => {
+app.post('/api/info', (req, res) => {
+  const {information} = req.body
+  console.log('THE POSTED MESSAGE WAS:', information)
+  res.status(201).json({"yourMessage": information})
+})
+
+app.put('/api', (req, res) => {
+  res.send('putting something')
+})
+
+app.delete('/delete/:id', (req, res) => {
   console.log('Want to delete something ?')
+  res.sendStatus(200)
 })
 
 app.listen(PORT, console.log(`Server running on port: ${PORT}`))
