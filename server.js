f3a2842208446b1b7887d2f4ce4c67c9df986b67
Tiago <tiagodev@protonmail.com>
2023-02-08 23:23:14 -0300

    adding rest client extension and crea a file to test the ports server

--- server.js
+++ server.js
@@ -2,4 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
+app.get('/', (req, res) => {
+  console.log('you are in Home route!: GET')
+  res.status(201).send('Hello')
+})
+
+app.delete('/', (req, res) => {
+  console.log('Want to delete something ?')
+})
+
 app.listen(PORT, console.log(`Server running on port: ${PORT}`))
